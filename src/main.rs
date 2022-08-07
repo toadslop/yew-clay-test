429c84823fad7aff4bba016adf64b9ff33a63dcc
Brian Heise <92563897+bnheise@users.noreply.github.com>
2022-08-07 11:06:20 +0900

    fixed the svgs

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,31 @@
 use std::rc::Rc;
 
-use gloo_console::log;
-use material_yew::MatButton;
-use yew::prelude::*;
-use yew_clay::button::ButtonGroup;
-use yew_clay::button::{ClayButton, DisplayType};
+use web_sys::MouseEvent;
+use yew::{html, Callback, Component, Context, Html};
+use yew_clay::button::{
+    ClayButton, ClayButtonGroup, ClayButtonProps, ClayButtonWithIcon, DisplayType,
+};
 use yew_clay::icon::ClayIcon;
 use yew_dom_attributes::attributes::aria_attributes::AriaAttributes;
 use yew_dom_attributes::attributes::button_html_attributes::ButtonHtmlAttributes;
 use yew_dom_attributes::events::events::{EventType, MouseEvents};
 use yew_dom_attributes::props::aria_props::AriaPropsHandler;
-use yew_dom_attributes::props::button_props::{ButtonProps2, ButtonPropsHandler};
+use yew_dom_attributes::props::button_props::{ButtonProps, ButtonPropsHandler};
 use yew_dom_attributes::props::custom_attributes::{CustomAttribute, CustomPropsHandler};
 use yew_dom_attributes::props::DomInjector;
 
 pub enum Msg {
-    AddOne,
-    SetDisabled(bool),
+    ToggleDisabled,
     RemoveListener(String),
-    UpdateButtonProps(Rc<ButtonProps2>),
+    UpdateBtnPrimaryProps(Rc<ButtonProps>),
+    UpdateBtnWarningProps(Rc<ButtonProps>),
 }
 
 struct Model {
-    value: i64,
     btn_disabled: bool,
-    button_props: Rc<ButtonProps2>,
+    button_primary_props: Rc<ButtonProps>,
+    button_warning_props: Rc<ButtonProps>,
+    icon_button_props: ClayButtonProps,
 }
 
 impl Component for Model {
@@ -32,92 +33,107 @@ impl Component for Model {
     type Properties = ();
 
     fn create(ctx: &Context<Self>) -> Self {
-        let on_btn_props_update: Callback<Rc<ButtonProps2>> =
-            ctx.link().callback(move |btn_props: Rc<ButtonProps2>| {
-                gloo_console::log!("CALLBACK EMITTED");
-                Msg::UpdateButtonProps(btn_props)
-            });
-        let mut button_props = Rc::new(ButtonProps2::new(on_btn_props_update));
-        Rc::make_mut(&mut button_props).add_aria_prop(AriaAttributes::AriaAtomic(true));
-        gloo_console::log!("create ran");
+        let on_btn_props_update: Callback<Rc<ButtonProps>> = ctx
+            .link()
+            .callback(move |btn_props: Rc<ButtonProps>| Msg::UpdateBtnPrimaryProps(btn_props));
 
-        let callback: Callback<MouseEvent> = ctx.link().callback(move |_ev| {
-            log!("something");
-            Msg::RemoveListener("click-event".into())
-        });
-        Rc::make_mut(&mut button_props).add_listener(
+        let mut button_primary_props = Rc::new(ButtonProps::new(on_btn_props_update));
+
+        Rc::make_mut(&mut button_primary_props).add_aria_prop(AriaAttributes::AriaAtomic(true));
+
+        let callback: Callback<MouseEvent> = ctx
+            .link()
+            .callback(move |_ev| Msg::RemoveListener("click-event".into()));
+
+        Rc::make_mut(&mut button_primary_props).add_listener(
             "click-event".into(),
             EventType::MouseEvent(MouseEvents::Click(callback)),
         );
 
+        let on_btn_warning_props_update: Callback<Rc<ButtonProps>> = ctx
+            .link()
+            .callback(move |btn_props: Rc<ButtonProps>| Msg::UpdateBtnWarningProps(btn_props));
+
+        let button_warning_props = Rc::new(ButtonProps::new(on_btn_warning_props_update));
+
+        let callback: Callback<MouseEvent> = ctx.link().callback(move |_ev| Msg::ToggleDisabled);
+
+        Rc::make_mut(&mut button_primary_props).add_listener(
+            "set-disabled".into(),
+            EventType::MouseEvent(MouseEvents::Click(callback)),
+        );
+        button_warning_props
+            .get_props_update_callback()
+            .emit(button_warning_props.clone());
+
+        let icon_button_props = ClayButtonProps {
+            borderless: true,
+            ..Default::default()
+        };
+
         Self {
-            value: 0,
             btn_disabled: false,
-            button_props,
+            button_primary_props,
+            button_warning_props,
+            icon_button_props,
         }
     }
 
     fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
         match msg {
-            Msg::AddOne => {
-                self.value += 1;
-                // the value has changed so we need to
-                // re-render for it to appear on the page
-                true
-            }
-            Msg::SetDisabled(is_disabled) => {
-                log!("set disabled ran");
-                Rc::make_mut(&mut self.button_props)
-                    .add_button_prop(ButtonHtmlAttributes::Disabled);
-                self.btn_disabled = is_disabled;
-                Rc::make_mut(&mut self.button_props).add_custom_prop(CustomAttribute::new(
+            Msg::ToggleDisabled => {
+                self.btn_disabled = !self.btn_disabled;
+
+                if self.btn_disabled {
+                    Rc::make_mut(&mut self.button_warning_props)
+                        .add_button_prop(ButtonHtmlAttributes::Disabled);
+                } else {
+                    Rc::make_mut(&mut self.button_warning_props)
+                        .remove_button_prop(ButtonHtmlAttributes::Disabled);
+                }
+
+                Rc::make_mut(&mut self.button_warning_props).add_custom_prop(CustomAttribute::new(
                     "my-custom-attribute".into(),
                     "lalalala".into(),
                 ));
                 true
             }
             Msg::RemoveListener(key) => {
-                gloo_console::log!("REMOVING IT");
-                Rc::make_mut(&mut self.button_props).remove_listener(key);
+                Rc::make_mut(&mut self.button_primary_props).remove_listener(key);
                 true
             }
-            Msg::UpdateButtonProps(new_props) => {
-                gloo_console::log!("update props");
-                self.button_props = new_props;
+            Msg::UpdateBtnPrimaryProps(new_props) => {
+                self.button_primary_props = new_props;
+                false
+            }
+            Msg::UpdateBtnWarningProps(new_props) => {
+                self.button_warning_props = new_props;
                 false
             }
         }
     }
 
-    // fn changed(&mut self, ctx: &Context<Self>) -> bool {}
-
-    fn view(&self, ctx: &Context<Self>) -> Html {
-        gloo_console::log!("view ran");
-        let link = ctx.link();
-        let is_disabled = self.btn_disabled.to_owned();
-
-        let spritemap = "https://cdn.jsdelivr.net/npm/@clayui/css/lib/images/icons/icons.svg";
+    fn view(&self, _ctx: &Context<Self>) -> Html {
+        let spritemap = "static/icons.svg";
 
         html! {
             <div>
-                <button onclick={link.callback(|_| Msg::AddOne)}>{ "+1" }</button>
-                <p data-key={"something"} > { self.value }</p>
-                <MatButton label="Click me!"  />
-                <ButtonGroup spaced={true} class={"stupid-class"}>
+                <h1>{ "Yew Component Library With Dynamic Props Demo" }</h1>
+                <ClayButtonGroup spaced={true} class={"stupid-class"}>
                     <ClayButton
                         display_type={DisplayType::Info}
-                        button_html_attributes={Some(self.button_props.clone())}
+                        button_html_attributes={Some(self.button_primary_props.clone())}
                         >
                     {"Click Me"}
                     </ClayButton>
                     <ClayButton
                         display_type={DisplayType::Warning}
-                        >
+                        button_html_attributes={Some(self.button_warning_props.clone())}
+                         >
                     {"Other Button"}
                     </ClayButton>
-
-                </ButtonGroup>
-                <ClayIcon spritemap={spritemap} symbol={"add-cell"}></ClayIcon>
+                    <ClayButtonWithIcon clay_button_props={self.icon_button_props.clone()} spritemap={spritemap} symbol={"add-cell"} />
+                </ClayButtonGroup>
             </div>
         }
     }
